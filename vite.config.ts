fc1fa504b8f0880ceb73f8159e22a614fc619a85
Muhaysin Muqtar <muhaysinmuqtar342@gmail.com>
2026-05-20 17:43:50 +0000

    Fix: Resolved 500 error by dumping autoloader and updated Vite config for CORS/HTTPS.

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
-        host: '127.0.0.1',
+        host: '0.0.0.0',
+        cors: true,
+        hmr: {
+            host: 'sadiq-autopart.test',
+        },
     },
     plugins: [
         laravel({
